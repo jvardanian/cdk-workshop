0ee43bb510d64d765324e825d7b3edb722caf74b
John Vardanian <jvardanian@gmail.com>
2022-05-14 14:56:26 -0700

    add an API Gateway in front of lambda function

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,7 +1,8 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
-    aws_lambda as _lambda
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
 
 
@@ -16,4 +17,9 @@ class CdkWorkshopStack(Stack):
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('lambda'),
             handler='hello.handler'
+        )
+
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=my_lambda
         )
