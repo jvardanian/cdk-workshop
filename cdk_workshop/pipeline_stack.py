c1287134fb0ae8075a3a88a6012394fd1bae2359
John Vardanian <jvardanian@gmail.com>
2022-05-14 20:44:01 -0700

    add CodePipeline to stack

---
+++ cdk_workshop/pipeline_stack.py
@@ -0,0 +1,34 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_codecommit as codecommit,
+    pipelines as pipelines,
+)
+
+
+class WorkshopPipelineStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Creates a CodeCommit repository called 'WorkshopRepo'
+        repo = codecommit.Repository(
+            self, 'WorkshopRepo',
+            repository_name="WorkshopRepo"
+        )
+
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Pipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=pipelines.CodePipelineSource.code_commit(repo, "main"),
+                commands=[
+                    "npm install -g aws-cdk",
+                    # Installs the cdk cli on Codebuild
+                    "pip install -r requirements.txt",
+                    # Instructs Codebuild to install required packages
+                    "npx cdk synth",
+                ]
+            ),
+        )
